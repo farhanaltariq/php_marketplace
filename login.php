7e672a133cd784b65083655878095ee5c61f89ed
hanpetualang <farhanaaxy@gmail.com>
2021-06-21 01:12:29 +0700

    create index (home) page

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
 <head><title>AeroStreet Login</title>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-<link rel="stylesheet" href="./style/homeStyle.css">
+<link rel="stylesheet" href="./style/loginStyle.css">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark">
@@ -17,21 +17,32 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav ml-auto">
-            <a class="nav-link active"  href="#">HOME</a>
+            <a class="nav-link"  href="#">HOME</a>
             <a class="nav-link" aria-current="page" href="#">PRODUCT</a>
-            <a class="nav-link" href="#">LOGIN</a>
+            <a class="nav-link active" href="#">LOGIN</a>
             <a class="nav-link" href="#" tabindex="-1">CONTACT</a>
             <img src="./style/img/bag.png" style="width: 25px; height: 25px; margin-top: 5px; margin-left: 20px">
         </div>
         </div>
     </div>
     </nav>
-    <div class="container position-absolute top-50 start-50 translate-middle text-center">
-        <div class="jumbotron">
-            <img src="./style/img/logo1.png" alt="" class="brand">
-            <br>
-            <a class="btn btn-lg" href="login.php" role="button"><b>SHOP NOW</b></a>
+    <div class="loginradius container position-absolute top-50 start-50 translate-middle">
+        <div class="loginlogo position-absolute top-0 start-0 text-center"">
         </div>
+        <div class="loginform">
+            <div class="text-left" style="margin-bottom: 50px;"><b>Login Here !</b></div>
+            <form action="home.php" method="post">
+                <div class="mb-3">
+                    <input placeholder="Email" type="email" class="form-control field rounded" id="exampleInputEmail1" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                    <input placeholder="Password" type="password" class="form-control field" id="exampleInputPassword1">
+                </div>
+                <a href="hehe.php">Forgot your password ?</a><button style="margin-left: 100px" type="submit" class="btn btn-light">Submit</button>
+            </form>
+        </div>
+        <div class="loginform">
+            Don't have an account ? create your account <a style="color:orange" href="home.php">here</a>
         </div>
     </div>
 </body>
