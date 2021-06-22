e08cebe8f19325af1c61271ff89ea623f2879baa
hanpetualang <farhanaaxy@gmail.com>
2021-06-22 15:31:28 +0700

    simplify sidebar

--- home.php
+++ home.php
@@ -10,13 +10,14 @@
     </head>
     <body>
     <style type="text/css">
-        h1, h4 {
+        h1 {
             text-align: center;
-            font-size: 120px;
+            font-size: 90px;
             font-family: Helvetica, Verdana, Arial;
         }
     </style>
     <h1>Sabar broh, belom jadi</h1>
-    <h4><a href="logout.php">Log Out</a></h4>
+    <h1><a href="index.php">Home</a></h1>
+    <h1><a href="logout.php">Log Out</a></h1>
     </body>
 </html>
--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
     include_once("connection.php");
+    include_once("navbar.php");
 ?>
 <!DOCTYPE html>
 <html>
-<head><title>AeroStreet Login</title>
+<head><title>AeroStreet</title>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 <link rel="stylesheet" href="./style/homeStyle.css">
@@ -11,36 +12,7 @@
 <script src="./style/sidebar.js"></script>
 </head>
 <body>
-        <!--Navigation Bar  & Side Bar-->
-        <div class="container-fluid">
-            <!-- Side Bar -->
-            <nav class="navbar navbar-expand-lg navbar-dark">
-            <div id="mySidenav" class="sidenav">
-                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-                <a href="#">My Account</a>
-                <a href="#">Product</a>
-                <a href="#">My Bag</a>
-                <a href="#">My Order</a>
-            </div>
-            <span class="stripes" style="font-size:30px; margin-left:20px; margin-right: 20px" onclick="openNav()">&#9776;</span>
-            <img src="./style/img/logo2.png" class="logo" alt="AeroStreet">
-            <!-- Navigation Bar -->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse ml-auto" id="navbarTogglerDemo02">
-                <div class="navbar-nav ml-auto">
-                    <a style="margin-left: 50px;" class="nav-link active"  href="index.php">HOME</a>
-                    <a style="margin-left: 50px;" class="nav-link" aria-current="page" href="home.php">PRODUCT</a>
-                    <a style="margin-left: 50px;" class="nav-link" href="login.php">LOGIN</a>
-                    <a style="margin-left: 50px;" class="nav-link" href="https://api.whatsapp.com/send?phone=6281230447023&text=Hai%20Farhan,%20kamu%20sangat%20tampan.">CONTACT</a>
-                </div>
-                <img src="./style/img/bag.png" style="width: 25px; height: 25px; margin-top: 5px; margin-left: 50px; margin-right: 50px;">
-            </div>
-            </nav>
-        </div>
-
-        <!--BUTTON-->
+    <!--BUTTON-->
     <div class="container position-absolute top-50 start-50 translate-middle text-center">
         <div class="jumbotron">
             <img src="./style/img/logo1.png" alt="" class="brand">
