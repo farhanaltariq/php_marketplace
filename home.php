69a2c5e0c904300369ee83eaa0b71512018c33ed
hanpetualang <farhanaaxy@gmail.com>
2021-07-13 22:32:14 +0700

    add check user function

--- home.php
+++ home.php
@@ -1,10 +1,15 @@
 <?php
 session_start();
 require 'connection.php';
+
 $produk = mysqli_query($connect, "SELECT * FROM product");
 if (!isset($_SESSION['userweb']))
     header("location: index.php");
+else
+    $email = $_POST['mail'];
 include_once("navbar.php");
+if(isset($_POST['addtocart']))
+    echo "YOLO";
 ?>
 <html>
 
@@ -28,13 +33,19 @@ include_once("navbar.php");
         <div class="row">
             <?php foreach ($produk as $row) : ?>
                 <div class="card" style="width: 18rem;">
+                <form action="" method="POST">
                     <img width="150" height="200" src="<?= $row['img'] ?>" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h4><b><?= $row["tipe"]; ?></b></h4>
                         <p><?= $row["harga"]; ?></p>
                         <p><?= $row["ukuran"]; ?></p>
                         <p><?= $row["stok"]; ?></p>
+                        <div style="text-align: right; font-size: 24px;">
+                        <input type="mail" name="mail" value="$email" hidden>
+                        <button type='submit' name='addtocart'><b>&plus;</a></b></button>
+                        </div>
                     </div>
+                    </form>
                 </div>
             <?php endforeach; ?>
         </div>
