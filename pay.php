7bb83bb36fae35a3facb6d9d597a1facbfcc406e
hanpetualang <farhanaaxy@gmail.com>
2021-07-19 00:01:54 +0700

    create payment page

--- pay.php
+++ pay.php
@@ -28,15 +28,17 @@
           $var3 = md5($var3);   // convert $var3 using md5 function and generate 32 characters hex number
       
           $fnm = $_FILES["image"]["name"];    // get the image name in $fnm variable
-          $dst = "./all_images/payment".$var3.$fnm;  // storing image path into the {all_images} folder with 32 characters hex number and file name
-          $dst_db = "all_images/payment".$var3.$fnm; // storing image path into the database with 32 characters hex number and file name
+          $dst = "./all_images/payment/".$var3.$fnm;  // storing image path into the {all_images} folder with 32 characters hex number and file name
+          $dst_db = "all_images/payment/".$var3.$fnm; // storing image path into the database with 32 characters hex number and file name
       
           move_uploaded_file($_FILES["image"]["tmp_name"],$dst);  // move image into the {all_images} folder with 32 characters hex number and image name
         
           $check = mysqli_query($connect,"insert into product(tipe, harga, ukuran, stok,img) values('$_POST[Type]', '$_POST[Price]', '$_POST[Size]', '$_POST[Stock]','$dst_db')");  // executing insert query
           
-          if($check)
-            header('location:admin.php');
+          if($check){
+            echo "<script>alert('Thank You, Your Order Will be Processed Soon');</script>";
+            echo "<script>window.location.replace('mybag.php');</script>";
+          }
         
           mysqli_close($db);  // close connection 
         }
